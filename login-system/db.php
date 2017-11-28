0808911cb30ed20db1122e5637829267a28ab56f
Jason Xiong <JasonJianXiong@gmail.com>
2017-11-28 15:27:43 -0500

    First commit

---
+++ login-system/db.php
@@ -0,0 +1,8 @@
+<?php
+/* Database connection settings */
+$host = 'localhost';
+$user = 'jianxio3_jason';
+$pass = 'slby19900917';
+$db = 'jianxio3_x5142044';
+$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
+?>
